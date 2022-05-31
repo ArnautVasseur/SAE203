4db251cea9478cb1b1423ea80ce717f81c39e0ee
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-05-31 13:58:07 +0200

    Creating vue and path for StyleGuideView

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import ConcertView from '../views/ConcertView.vue'
 import ContactView from '../views/ContactView.vue'
 import MentionsLégalesView from '../views/MentionsLégalesView.vue'
 import BilleterieView from '../views/BilleterieView.vue'
+import StyleGuideView from '../views/StyleGuideView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,8 @@ const router = createRouter({
     { path: '/mentionslegales',   name: 'mentionslégales',    component: MentionsLégalesView},
     { path: '/billeterie',   name: 'billeterie',    component: BilleterieView},
     { path: "/:catchAll(.*)", redirect: '/404',},
-    { path: '/404', name: 'PageNotExist', component: () => import('../views/Page404.vue')}
+    { path: '/404', name: 'PageNotExist', component: () => import('../views/Page404.vue')},
+    { path: '/styleguide',   name: 'styleguide',    component: StyleGuideView},
   ]
 })
 
